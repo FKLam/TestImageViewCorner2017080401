a1bbec431d7a51289a8f9424242e62ed7f45dc72
林房坤 <1783956729@qq.com>
2017-08-04 11:10:43 +0800

    添加项目

---
+++ TestImageViewCorner2017080401/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestImageViewCorner2017080401
+//
+//  Created by amglfk on 2017/8/4.
+//  Copyright © 2017年 amglfk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestImageViewCorner2017080401/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestImageViewCorner2017080401
+//
+//  Created by amglfk on 2017/8/4.
+//  Copyright © 2017年 amglfk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
